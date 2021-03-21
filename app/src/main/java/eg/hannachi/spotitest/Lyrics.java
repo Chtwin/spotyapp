8ccb72d6d6a376a3375668b81ed63bbb0940f2f5
Chtwin <37251146+Chtwin@users.noreply.github.com>
2021-03-21 15:39:51 +0100

    Revert "add comments"
    This reverts commit fcfd0e0ba353c68dc1da30a602b1a210ba7c7d22.

--- app/src/main/java/eg/hannachi/spotitest/Lyrics.java
+++ app/src/main/java/eg/hannachi/spotitest/Lyrics.java
@@ -1,21 +1,27 @@
 package eg.hannachi.spotitest;
 
-import android.annotation.SuppressLint;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class Lyrics extends AsyncTask<String, Void, String> {
-    @SuppressLint("StaticFieldLeak")
-    private final AppCompatActivity app;
+    private AppCompatActivity app;
+    ArrayList<String> list = new ArrayList<String>();
 
     public Lyrics(AppCompatActivity a){
         this.app = a;
@@ -27,13 +33,17 @@ public class Lyrics extends AsyncTask<String, Void, String> {
         String j = null;
         try {
             url = new URL(strings[0]);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection(); // Create the HTTP connection
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            Log.i("JJJ", url.toString());
             try {
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream()); // Get the stream and put it in a buffer before reading
-                j = readStream(in); // read the data and get the lyrics
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                j = readStream(in);
+                //Log.i("JJJ3", j.toString());
             } finally {
                 urlConnection.disconnect();
             }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -43,8 +53,8 @@ public class Lyrics extends AsyncTask<String, Void, String> {
     @Override
     protected void onPostExecute(String j) {
         super.onPostExecute(j);
-        TextView lyr = app.findViewById(R.id.parole); // get the lyrics field in the actvity
-        lyr.setText(j); // Set the lyrics
+        TextView parolefield = app.findViewById(R.id.parole);
+        parolefield.setText(j);
     }
 
     private String readStream(InputStream is) throws IOException {
@@ -54,32 +64,32 @@ public class Lyrics extends AsyncTask<String, Void, String> {
             bo.write(i);
             i = is.read();
         }
-        // It's all the HTML code of the web page
-        String s_original = bo.toString();
-        // Only the tag with the lyrics is kept
+        String s_original = bo.toString(); //tt le code html
+        //on garde que la balise lyrics
         int index = s_original.indexOf("<div class=\"lyrics\">")-1;
         String s1 = s_original.substring(index);
         int index2= s1.indexOf("</div>")-1;
         String s2;
-        // Only the p tag is kept, because it s the paragraphs with le lyrics
+        //on garde que le paragraphe p qui a les paroles
         index = s_original.indexOf("<p>")+3;
         s1 = s_original.substring(index);
         index2= s1.indexOf("</p>");
         s2 = s1.substring(0, index2);
-        if (s2.contains("[Paroles")){ // remove the line where the title of the music is specified
+
+        if (s2.contains("[Paroles")){
             index = s2.indexOf("[Paroles");
             s1 = s2.substring(index);
             index2 = s1.indexOf("]")+1;
             String s3 = s1.substring(0, index2);
             s2 = s2.replace(s3, "");
         }
-        while (s2.contains("<")){ // remove all the html tag in the lyrics
+        while (s2.contains("<")){
             index = s2.indexOf("<");
             s1 = s2.substring(index);
             index2 = s1.indexOf(">")+1;
             String s3 = s1.substring(0, index2);
             s2 = s2.replace(s3, "\n");
         }
-        return s2; // return the clean lyrics
+        return s2;
     }
 }
--- app/src/main/java/eg/hannachi/spotitest/LyricsURL.java
+++ app/src/main/java/eg/hannachi/spotitest/LyricsURL.java
@@ -2,6 +2,8 @@ package eg.hannachi.spotitest;
 
 import android.annotation.SuppressLint;
 import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,33 +14,37 @@ import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
-@SuppressLint("StaticFieldLeak")
 public class LyricsURL extends AsyncTask<String, Void, JSONObject> {
 
-    private final AppCompatActivity app;
+    private AppCompatActivity app;
+    ArrayList<String> list = new ArrayList<String>();
 
     public LyricsURL(AppCompatActivity a){
         this.app = a;
     }
 
-    @SuppressLint("WrongThread")
     @Override
     protected JSONObject doInBackground(String... strings) {
         URL url = null;
         JSONObject j = null;
         try {
             url = new URL(strings[0]);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection(); // Create the HTTP connection
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            Log.i("JJJ", url.toString());
             try {
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream()); // Get the stream and put it in a buffer before reading
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
                 j = readStream(in);
                 String url_lyrics = j.getJSONObject("response").getJSONArray("hits").getJSONObject(0).getJSONObject("result").getString("url");
                 AsyncTask<String, Void, String> task = new Lyrics(this.app);
-                task.execute(url_lyrics, null, null); // performs the task that will retrieve the lyrics
+                task.execute(url_lyrics, null, null);
+                //Log.i("JJJ3", j.getString("lyrics"));
+                //TextView parolefield = app.findViewById(R.id.paroles);
+                //parolefield.setText(j.getString("lyrics"));
             } catch (JSONException e) {
                 e.printStackTrace();
             } finally {
@@ -50,7 +56,6 @@ public class LyricsURL extends AsyncTask<String, Void, JSONObject> {
         return new JSONObject();
     }
 
-    // This method convert the data of the bufferes in JsonObject
     private JSONObject readStream(InputStream is) throws IOException {
         try{
             ByteArrayOutputStream bo = new ByteArrayOutputStream();
@@ -59,6 +64,7 @@ public class LyricsURL extends AsyncTask<String, Void, JSONObject> {
                 bo.write(i);
                 i = is.read();
             }
+            //Log.i("JJJ2", bo.toString());
             return new JSONObject(bo.toString());
         } catch (JSONException e) {
             return new JSONObject();
