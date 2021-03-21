274cf7e23655daa0c8830713193fe0a2e163f6ea
Chtwin <37251146+Chtwin@users.noreply.github.com>
2021-03-21 15:40:20 +0100

    Revert "Revert "add comments""
    This reverts commit 8ccb72d6d6a376a3375668b81ed63bbb0940f2f5.

--- app/src/main/java/eg/hannachi/spotitest/Lyrics.java
+++ app/src/main/java/eg/hannachi/spotitest/Lyrics.java
@@ -1,27 +1,21 @@
 package eg.hannachi.spotitest;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONStringer;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 
 public class Lyrics extends AsyncTask<String, Void, String> {
-    private AppCompatActivity app;
-    ArrayList<String> list = new ArrayList<String>();
+    @SuppressLint("StaticFieldLeak")
+    private final AppCompatActivity app;
 
     public Lyrics(AppCompatActivity a){
         this.app = a;
@@ -33,17 +27,13 @@ public class Lyrics extends AsyncTask<String, Void, String> {
         String j = null;
         try {
             url = new URL(strings[0]);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            Log.i("JJJ", url.toString());
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection(); // Create the HTTP connection
             try {
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                j = readStream(in);
-                //Log.i("JJJ3", j.toString());
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream()); // Get the stream and put it in a buffer before reading
+                j = readStream(in); // read the data and get the lyrics
             } finally {
                 urlConnection.disconnect();
             }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -53,8 +43,8 @@ public class Lyrics extends AsyncTask<String, Void, String> {
     @Override
     protected void onPostExecute(String j) {
         super.onPostExecute(j);
-        TextView parolefield = app.findViewById(R.id.parole);
-        parolefield.setText(j);
+        TextView lyr = app.findViewById(R.id.parole); // get the lyrics field in the actvity
+        lyr.setText(j); // Set the lyrics
     }
 
     private String readStream(InputStream is) throws IOException {
@@ -64,32 +54,32 @@ public class Lyrics extends AsyncTask<String, Void, String> {
             bo.write(i);
             i = is.read();
         }
-        String s_original = bo.toString(); //tt le code html
-        //on garde que la balise lyrics
+        // It's all the HTML code of the web page
+        String s_original = bo.toString();
+        // Only the tag with the lyrics is kept
         int index = s_original.indexOf("<div class=\"lyrics\">")-1;
         String s1 = s_original.substring(index);
         int index2= s1.indexOf("</div>")-1;
         String s2;
-        //on garde que le paragraphe p qui a les paroles
+        // Only the p tag is kept, because it s the paragraphs with le lyrics
         index = s_original.indexOf("<p>")+3;
         s1 = s_original.substring(index);
         index2= s1.indexOf("</p>");
         s2 = s1.substring(0, index2);
-
-        if (s2.contains("[Paroles")){
+        if (s2.contains("[Paroles")){ // remove the line where the title of the music is specified
             index = s2.indexOf("[Paroles");
             s1 = s2.substring(index);
             index2 = s1.indexOf("]")+1;
             String s3 = s1.substring(0, index2);
             s2 = s2.replace(s3, "");
         }
-        while (s2.contains("<")){
+        while (s2.contains("<")){ // remove all the html tag in the lyrics
             index = s2.indexOf("<");
             s1 = s2.substring(index);
             index2 = s1.indexOf(">")+1;
             String s3 = s1.substring(0, index2);
             s2 = s2.replace(s3, "\n");
         }
-        return s2;
+        return s2; // return the clean lyrics
     }
 }
--- app/src/main/java/eg/hannachi/spotitest/MainActivity.java
+++ app/src/main/java/eg/hannachi/spotitest/MainActivity.java
@@ -7,40 +7,32 @@ import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 
-import com.spotify.protocol.types.Artist;
 import com.spotify.protocol.types.Image;
-import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
 
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String CLIENT_ID = "adcfa4bfab734c7aa2bbffb1c63805bd"; //anis
+    private static final String CLIENT_ID = "adcfa4bfab734c7aa2bbffb1c63805bd"; // anis
     //private static final String CLIENT_ID = "bc064d26c0c44116be4f4b49da7f0118"; //gab
     private static final String REDIRECT_URI = "http://example.com/callback/";
+    private final String token = "_ubTsw2W0EC_ER_II73nI8er_Gdi0W65o3ITAP-TzxCLMe4FNUWP9nYaapeaark1"; // Token that we use for the connection to the Genius API
     private SpotifyAppRemote mSpotifyAppRemote;
     private TextView text;
     private ImageView img;
     private TextView art;
     private TextView album;
-    private ProgressBar pro;
 
 
     @Override
@@ -53,24 +45,25 @@ public class MainActivity extends AppCompatActivity {
         delegate.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         delegate.applyDayNight();
-        // End
+        // Declare of the module of the UI
         text = findViewById(R.id.text);
         img = findViewById(R.id.cover);
         art = findViewById(R.id.album);
         album = findViewById(R.id.artist);
         Button act = findViewById(R.id.act);
-        pro = findViewById(R.id.progressBar);
-        act.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), Activity2.class)));
+        act.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), Activity2.class))); // Clicking on the button redirect to another activity
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        // Set the parameter to create the connection with spotify server
         ConnectionParams connectionParams =
         new ConnectionParams.Builder(CLIENT_ID)
                 .setRedirectUri(REDIRECT_URI)
                 .showAuthView(true)
                 .build();
+        // Create the connection with the spotify server and store the data in the mSpotifyAppRemote attribut.
         SpotifyAppRemote.connect(this, connectionParams,
                 new Connector.ConnectionListener() {
                     @Override
@@ -91,15 +84,13 @@ public class MainActivity extends AppCompatActivity {
         mSpotifyAppRemote.getPlayerApi()
                 .subscribeToPlayerState()
                 .setEventCallback(playerState -> {
-                    final Track track = playerState.track;
-                    if (track != null) {
-                        //pro.setProgress((int)(playerState.playbackPosition / playerState.track.duration)*100);
+                    final Track track = playerState.track; // We get the track
+                    if (track != null) {    // Check if a music is playing
                         String titre = parseTitle(track.name);
-                        Log.d("MainActivity", titre + " by " + track.artist.name);
-                        String token = "_ubTsw2W0EC_ER_II73nI8er_Gdi0W65o3ITAP-TzxCLMe4FNUWP9nYaapeaark1";
-                        String url = "https://api.genius.com/search?access_token="+token+"&q="+titre+" "+track.artist.name;
+                        String url = "https://api.genius.com/search?access_token="+token+"&q="+titre+" "+track.artist.name;  // We create the URL to get the music with the API
                         LyricsURL task = new LyricsURL(MainActivity.this);
-                        task.execute(url, null, null);
+                        task.execute(url, null, null); // get the music lyrics
+                        // We display the data on the app
                         text.setText(track.name);
                         art.setText(track.artist.name);
                         album.setText(track.album.name);
@@ -115,11 +106,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String parseTitle(String titre){
-        List<String> l = Arrays.asList("(","(",")","[","[","]","remix","r","x","-","-","-");
+        List<String> l = Arrays.asList("(","(",")","[","[","]","remix","r","x","-","-","-"); // The character that we want to remove with their content
         int index, index2;
         String s3, s1;
         for (int i = 0; i < l.size(); i += 3){
             if (i<=6){
+                //removes the characters for which we know the beginning and the end
                 while (titre.contains(l.get(i))){
                     index = titre.indexOf(l.get(i+1))-1;
                     s1 = titre.substring(index);
@@ -128,17 +120,16 @@ public class MainActivity extends AppCompatActivity {
                     titre = titre.replace(s3, "");
                 }
             }
-            else{
-                if (titre.contains(l.get(i))){
-                    index = titre.indexOf(l.get(i+1))-1;
-                    s1 = titre.substring(index);
-                    index2 = s1.indexOf(l.get(i+1))+1;
-                    s3 = s1;
-                    titre = titre.replace(s3, "");
-                }
+            else if (titre.contains(l.get(i))){
+                //removes the characters whose beginning we know and from which we want to remove all the characters that follow
+                index = titre.indexOf(l.get(i+1))-1;
+                s1 = titre.substring(index);
+                index2 = s1.indexOf(l.get(i+1))+1;
+                s3 = s1;
+                titre = titre.replace(s3, "");
             }
         }
-        return  titre;
+        return titre; // return the parse title
     }
 
     @Override
